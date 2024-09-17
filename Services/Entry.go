56ae2c92b410f79778731f3a922f8b12011df6f3
システム君 <leonhardfresacher@gmail.com>
2024-09-17 14:38:35 +0200

    Entry Object

--- Services/Entry.go
+++ Services/Entry.go
@@ -1,1 +1,12 @@
 package Services
+
+type Entry struct {
+	Index    uint64
+	Title    string
+	Type     string
+	Rating   string
+	Started  string
+	Finished string
+	Status   string
+	Notes    string
+}
