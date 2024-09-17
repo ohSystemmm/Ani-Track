4c9d246dd4fdaa5e063253d3b917d74ca68334ff
Y2kun <me@y2kun.dev>
2024-09-17 18:15:29 +0200

    removed everything not relevant

--- Main.go
+++ Main.go
@@ -11,7 +11,6 @@ import (
 func main() {
 	output, _ := Services.Read()
 	fmt.Println(output)
-	p := btea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
