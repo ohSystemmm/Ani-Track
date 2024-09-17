a435489b3816d7a31ded07456e5c1e8d1f9e53ff
Y2kun <me@y2kun.dev>
2024-09-17 16:58:13 +0200

    tidied go dependecies, added an tutorial to App.go

--- Main.go
+++ Main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"Ani-Track/Services"
 	"fmt"
+	btea "github.com/charmbracelet/bubbletea"
+	// bzone "github.com/lrstanley/bubblezone"
+	"os"
 )
 
 func main() {
 	output, _ := Services.Read()
 	fmt.Println(output)
+	p := btea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
