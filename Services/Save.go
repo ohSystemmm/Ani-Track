dc3bcb2327b83663b645f1e5d0ebbcdc2108a849
システム君 <leonhardfresacher@gmail.com>
2024-09-24 11:27:21 +0200

    save

--- Services/Save.go
+++ Services/Save.go
@@ -1,5 +1,24 @@
 package Services
 
-func Save(content string, filepath string) {
+import (
+	"io/ioutil"
+	"os"
+)
 
+// Save takes JSON content as a string and a file path, and writes the content to the file.
+func Save(content string, filepath string) error {
+	// Create or overwrite the file
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write the content to the file
+	err = ioutil.WriteFile(filepath, []byte(content), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
