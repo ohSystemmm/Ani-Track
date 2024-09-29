4b99877531b2edff879dcbf14928c3768053dd87
システム君 <leonhardfresacher@gmail.com>
2024-09-29 19:41:22 +0200

    saving more efficient

--- Services/Download.go
+++ Services/Download.go
@@ -5,25 +5,25 @@ import (
 	"strings"
 )
 
-func Download(content string, filepath string) bool {
+func Download(content string, filepath string) error {
 	if strings.HasPrefix(filepath, "~/") {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
-			return false
+			return err
 		}
 		filepath = strings.Replace(filepath, "~", homeDir, 1)
 	}
 
 	file, err := os.Create(filepath)
 	if err != nil {
-		return false
+		return err
 	}
 	defer file.Close()
 
 	_, err = file.Write([]byte(content))
 	if err != nil {
-		return false
+		return err
 	}
 
-	return true
+	return nil
 }
